d933fc99a49aa7c2238a9c2cafb039bf140e2ec4
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2025-05-27 15:35:07 +0530

    Update Q4.cs

--- Week 02 Day 02 - 27 May 25/Q4.cs
+++ Week 02 Day 02 - 27 May 25/Q4.cs
@@ -1,1 +1,54 @@
- 
+using System;
+using System.Diagnostics.Contracts;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try 
+        {
+            double price = double.Parse(Console.ReadLine());
+            if(price<=0)
+            {
+                throw new InvalidPriceException("Price must be greater than zero.");
+            }
+
+            int quantity = int.Parse(Console.ReadLine());
+            if(quantity < 0)
+            {
+                throw new InvalidQuantityException("Quantity must be greater than zero.");
+            }
+            double Total = price* quantity;
+            Console.WriteLine($"Total cost is {Total}");
+        }
+        catch(InvalidPriceException e1)
+        {
+            Console.WriteLine($"Error: {e1.Message}");
+        }
+        catch(InvalidQuantityException e2)
+        {
+            Console.WriteLine($"Error: {e2.Message}");
+        }
+        catch(FormatException e3)
+        {
+            Console.WriteLine("Error: Please enter a valid number.");
+        }
+    }
+}
+
+
+public class InvalidPriceException : Exception
+{
+    public InvalidPriceException(string msg):base(msg)
+    {
+
+    }
+}
+
+public class InvalidQuantityException : Exception
+{
+    public InvalidQuantityException(string msg) : base(msg)
+    {
+
+    }
+}
