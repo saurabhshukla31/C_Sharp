37a1166d8ae936f39cb2c6f833ff3defdb74e7ff
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2025-05-27 15:41:48 +0530

    Update Q5.cs

--- Week 02 Day 02 - 27 May 25/Q5.cs
+++ Week 02 Day 02 - 27 May 25/Q5.cs
@@ -1,1 +1,43 @@
+using System;
 
+public class InvalidStringException: Exception
+{
+    public InvalidStringException(): base("Invalid string privided."){}
+    public InvalidStringException(string message):base(message){}
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            string input = Console.ReadLine();
+            foreach(char c in input)
+            {
+                if(!char.IsLetter(c))
+                {
+                    throw new InvalidStringException("String must contain only alphabetic characters.");
+                }
+            }
+            int vowelCount = 0;
+            string vowels = "aeiouAEIOU";
+            foreach(char c in input)
+            {
+                if(vowels.Contains(c))
+                {
+                    vowelCount++;
+                }
+            }
+            Console.WriteLine($"Number of vowels: {vowelCount}");
+        }
+        catch(InvalidStringException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error: An unexpected error occcured. {ex.Message}");
+        }
+    }
+}
