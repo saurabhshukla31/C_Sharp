fab87b1c43a9ab0ccfbce7780dcb1cbea75649a5
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2025-05-27 15:48:59 +0530

    Update Q9.cs

--- Week 02 Day 02 - 27 May 25/Q9.cs
+++ Week 02 Day 02 - 27 May 25/Q9.cs
@@ -1,1 +1,39 @@
+using System;
 
+public class Student
+{
+    public event Action<int> GradeChanged;
+    int grade;
+
+    public int Grade
+    {
+        get{
+            return grade;
+        }
+        set{
+            grade = value;
+            if(GradeChanged != null)
+            {
+                GradeChanged(grade);
+            }
+        }
+    }
+}
+public class Program
+{   
+    
+    public static void Main(string[] args)
+    {
+        Student s= new Student();
+        s.GradeChanged += (val) => Console.WriteLine($"Grade changed to: {val}");
+        bool Valid = int.TryParse(Console.ReadLine(),out int val);
+        if(Valid)
+        {
+            s.Grade = val;
+        }
+        else
+        {
+            Console.WriteLine("Invalid grade.");
+        }
+    }
+}
